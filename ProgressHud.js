fb0c2568566a8f081e9fb51cf1fcf839f436b265
Jeff Kirkell <jeff.kirkell@gmail.com>
2013-09-02 17:19:36 -0400

    updating cordova commands

--- ProgressHud.js
+++ ProgressHud.js
@@ -1,78 +1,81 @@
-//
-//  ProgressHud.js
-//
-// Created by Olivier Louvignes on 04/25/2012.
-//
-// Copyright 2011 Olivier Louvignes. All rights reserved.
-// MIT Licensed
+cordova.define("com.phonegap.plugins.progresshud.ProgressHud", function(require, exports, module) {/** 
+ * A plugin to enable iOS In-App Purchases.
+ *
+ * Copyright (c) Matt Kane 2011
+ * Copyright (c) Guillaume Charhon 2012
+ * Copyright (c) Jean-Christophe Hoelt 2013
+ */
 
-(function(cordova) {
+var exec = function (methodName, options, success, error) {
+    cordova.exec(success, error, "ProgressHud", methodName, options);
+};
 
-	function ProgressHud() {}
+var log = function (msg) {
+    console.log("ProgressHud[js]: " + msg);
+};
 
-	ProgressHud.prototype.show = function(options, callback) {
-		if(!options) options = {};
-		var scope = options.scope || null;
-		delete options.scope;
+var ProgressHud = function () {
+    this.options = {};
+};
 
-		var service = 'ProgressHud',
-			action = 'show',
-			callbackId = service + (cordova.callbackId + 1);
+ProgressHud.prototype.show = function(options, callback) {
+	if(!options) options = {};
+	var scope = options.scope || null;
+	delete options.scope;
 
-		var config = {
-			mode: options.mode || 'indeterminate',
-			labelText: options.labelText || 'Loading...',
-			detailsLabelText: options.detailsLabelText || '',
-			progress: options.progress || 0
-		};
+	var service = 'ProgressHud',
+		action = 'show',
+		callbackId = service + (cordova.callbackId + 1);
 
-		var _callback = function(result) {
-			if(typeof callback == 'function') callback.apply(scope, arguments);
-		};
-
-		return cordova.exec(_callback, _callback, service, action, [config]);
-
+	var config = {
+		mode: options.mode || 'indeterminate',
+		labelText: options.labelText || 'Loading...',
+		detailsLabelText: options.detailsLabelText || '',
+		progress: options.progress || 0
 	};
 
-	ProgressHud.prototype.set = function(options, callback) {
-		if(!options) options = {};
-		var scope = options.scope || null;
-		delete options.scope;
-
-		var service = 'ProgressHud',
-			action = 'set',
-			callbackId = service + (cordova.callbackId + 1);
-
-		var _callback = function(result) {
-			if(typeof callback == 'function') callback.apply(scope, arguments);
-		};
-
-		return cordova.exec(_callback, _callback, service, action, [options]);
-
+	var _callback = function(result) {
+		if(typeof callback == 'function') callback.apply(scope, arguments);
 	};
 
-	ProgressHud.prototype.hide = function(options, callback) {
-		if(!options) options = {};
-		var scope = options.scope || null;
-		delete options.scope;
+	return exec(action, [config], _callback, _callback);
+};
 
-		var service = 'ProgressHud',
-			action = 'hide',
-			callbackId = service + (cordova.callbackId + 1);
+ProgressHud.prototype.set = function(options, callback) {
+	if(!options) options = {};
+	var scope = options.scope || null;
+	delete options.scope;
 
-		var config = {};
+	var service = 'ProgressHud',
+		action = 'set',
+		callbackId = service + (cordova.callbackId + 1);
 
-		var _callback = function(result) {
-			if(typeof callback == 'function') callback.apply(scope, arguments);
-		};
-
-		return cordova.exec(_callback, _callback, service, action, [config]);
-
+	var _callback = function(result) {
+		if(typeof callback == 'function') callback.apply(scope, arguments);
 	};
 
-	cordova.addConstructor(function() {
-		if(!window.plugins) window.plugins = {};
-		window.plugins.progressHud = new ProgressHud();
-	});
+	return exec(action, [options], _callback, _callback);
 
-})(window.cordova || window.Cordova);
+};
+
+ProgressHud.prototype.hide = function(options, callback) {
+	if(!options) options = {};
+	var scope = options.scope || null;
+	delete options.scope;
+
+	var service = 'ProgressHud',
+		action = 'hide',
+		callbackId = service + (cordova.callbackId + 1);
+
+	var config = {};
+
+	var _callback = function(result) {
+		if(typeof callback == 'function') callback.apply(scope, arguments);
+	};
+
+	return exec(action, [config], _callback, _callback);
+
+};
+
+module.exports = new ProgressHud();
+});
